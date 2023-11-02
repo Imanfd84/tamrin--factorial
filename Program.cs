825bb4f3f3996132f103c64a203af04007191c01
Imanfd84 <149057251+Imanfd84@users.noreply.github.com>
2023-11-02 02:05:04 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//iman fattahi
+//tamrin factoriel
+
+
+int number, fact = 1;
+
+
+
+Console.WriteLine(" enter a number");
+ number= int.Parse(Console.ReadLine());
+for (  int i =1; i<= number; i++) 
+{
+    
+    fact *= i;
+
+
+}
+
+Console.WriteLine(fact );
+
+
+
+
